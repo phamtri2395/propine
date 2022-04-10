c65f7eecf6180376c7f7ebdc471c9f9cbcbaf4a2
Tri Pham Minh <phamtri2395@gmail.com>
2022-04-11 06:07:22 +0700

    feat(logger): update console logger interface

--- libs/logger/src/console.logger.ts
+++ libs/logger/src/console.logger.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-console */
 
@@ -15,37 +16,37 @@ const Theme: Record<LogLevel, Chalk> = {
 };
 
 export class ConsoleLogger extends LoggerService {
-  public log(message: string): void {
+  public log(message: any): void {
     const theme = Theme[LogLevel.LOG];
 
     console.log(theme(message));
   }
 
-  public info(message: string): void {
+  public info(message: any): void {
     const theme = Theme[LogLevel.INFO];
 
     console.log(theme(message));
   }
 
-  public warn(message: string): void {
+  public warn(message: any): void {
     const theme = Theme[LogLevel.WARN];
 
     console.log(theme(message));
   }
 
-  public error(message: string): void {
+  public error(message: any): void {
     const theme = Theme[LogLevel.ERROR];
 
     console.log(theme(message));
   }
 
-  public debug(message: string): void {
+  public debug(message: any): void {
     const theme = Theme[LogLevel.DEBUG];
 
     console.log(theme(message));
   }
 
-  public verbose(message: string): void {
+  public verbose(message: any): void {
     const theme = Theme[LogLevel.VERBOSE];
 
     console.log(theme(message));
